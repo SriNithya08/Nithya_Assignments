906dc6f16c44cbfcd4b77b073230ebcdba9e7d6a
SriNithya08 <153537802+SriNithya08@users.noreply.github.com>
2023-12-13 21:45:46 +0530

    Add files via upload
    Week1.Day1 - Assingnments

---
+++ Report.java
@@ -0,0 +1,10 @@
+
+public class Report {
+
+	public static void main(String[] args) {
+		Student stu = new Student();
+		stu.Data();
+
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,21 @@
+
+public class Student {
+	public void Data()
+	{
+	String studentName = "Nithya";
+	int rollNo = 102;
+	String collegeName = "Sri Krishna College of Arts and Science";
+	int markScored = 479;
+	float cgpa = 8.54f;
+		
+	System.out.println("STUDENT REPORT");
+	System.out.println("STUDENT NAME : "  + studentName);
+	System.out.println("STUDENT ROLL NO. : " + rollNo);
+	System.out.println("STUDENT COLLEGE NAME :  " + collegeName);
+	System.out.println("STUDENT TOTAL MARKS : " + markScored);
+	System.out.println("CGPA : " + cgpa);
+	
+}
+}
+
+
